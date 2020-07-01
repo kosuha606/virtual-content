4f69128b7f5e2a67ca393e730ab4de0614399d88
Kosuha John <kosuha606@yandex.ru>
2020-07-01 21:33:08 +0300

    seo page multilang

--- src/_routes/page.php
+++ src/_routes/page.php
@@ -8,6 +8,7 @@ use kosuha606\VirtualAdmin\Form\SecondaryFormService;
 use kosuha606\VirtualAdmin\Services\StringService;
 use kosuha606\VirtualAdmin\Structures\DetailComponents;
 use kosuha606\VirtualAdmin\Structures\ListComponents;
+use kosuha606\VirtualAdmin\Structures\SecondaryForms;
 use kosuha606\VirtualContent\Domains\Page\Models\PageVm;
 use kosuha606\VirtualModel\VirtualModel;
 use kosuha606\VirtualModelHelppack\ServiceManager;
@@ -89,116 +90,6 @@ return [
                     'additional_config' => function($model) {
                         $secondaryService = ServiceManager::getInstance()->get(SecondaryFormService::class);
 
-                        $config = $secondaryService->buildForm()
-                            ->setMasterModel($model)
-                            ->setMasterModelId($model->id.','.get_class($model))
-                            ->setMasterModelField('entity_id,entity_class')
-                            ->setRelationType(SecondaryFormBuilder::ONE_TO_ONE)
-                            ->setRelationClass(SeoPageVm::class)
-                            ->setTabName('SEO_data')
-                            ->setRelationEntities(SeoPageVm::many(['where' => [
-                                ['=', 'entity_id', $model->id],
-                                ['=', 'entity_class', get_class($model)]
-                            ]]))
-                            ->setConfig(function ($inModel) use ($model) {
-                                $stringService = ServiceManager::getInstance()->get(StringService::class);
-
-                                return [
-                                    [
-                                        'field' => 'id',
-                                        'label' => '',
-                                        'component' => DetailComponents::HIDDEN_FIELD,
-                                        'value' => $inModel->id,
-                                    ],
-                                    [
-                                        'field' => 'entity_id',
-                                        'label' => '',
-                                        'component' => DetailComponents::HIDDEN_FIELD,
-                                        'value' => $model->id,
-                                    ],
-                                    [
-                                        'field' => 'entity_class',
-                                        'label' => '',
-                                        'component' => DetailComponents::HIDDEN_FIELD,
-                                        'value' => get_class($model),
-                                    ],
-                                    [
-                                        'field' => 'title',
-                                        'label' => 'Заголовок',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->title,
-                                    ],
-                                    [
-                                        'field' => 'meta_keywords',
-                                        'label' => 'Мета ключевые слова',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->meta_keywords,
-                                    ],
-                                    [
-                                        'field' => 'mata_description',
-                                        'label' => 'Мета описание',
-                                        'component' => DetailComponents::TEXTAREA_FIELD,
-                                        'value' => $inModel->mata_description,
-                                    ],
-                                    [
-                                        'field' => 'og_title',
-                                        'label' => 'OG заголовок',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->og_title,
-                                    ],
-                                    [
-                                        'field' => 'og_description',
-                                        'label' => 'OG описание',
-                                        'component' => DetailComponents::TEXTAREA_FIELD,
-                                        'value' => $inModel->og_description,
-                                    ],
-                                    [
-                                        'field' => 'og_url',
-                                        'label' => 'OG адресс',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->og_url,
-                                    ],
-                                    [
-                                        'field' => 'og_image',
-                                        'label' => 'OG изображение',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->og_image,
-                                    ],
-                                    [
-                                        'field' => 'og_type',
-                                        'label' => 'OG тип',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->og_type,
-                                    ],
-                                    [
-                                        'field' => 'canonical',
-                                        'label' => 'Canonical',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->canonical,
-                                    ],
-                                    [
-                                        'field' => 'noindex',
-                                        'label' => 'Noindex',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->noindex,
-                                    ],
-                                    [
-                                        'field' => 'sitemap_importance',
-                                        'label' => 'Sitemap приоритет',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->sitemap_importance,
-                                    ],
-                                    [
-                                        'field' => 'sitemap_freq',
-                                        'label' => 'Sitemap частота обновления',
-                                        'component' => DetailComponents::INPUT_FIELD,
-                                        'value' => $inModel->sitemap_freq,
-                                    ],
-                                ];
-                            })
-                            ->getConfig()
-                        ;
-
                         $configComment = $secondaryService
                             ->buildForm()
                             ->setMasterModel($model)
@@ -248,7 +139,7 @@ return [
                         ;
 
                         return [
-                            $config,
+                            SecondaryForms::SEO_PAGE($model),
                             $configComment,
                         ];
                     },
